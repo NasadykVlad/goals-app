d76129899e757b21aa335da07921a5826f18248c
Nasadyk Vlad <t.empire228@gmail.com>
2023-12-20 13:03:03 +0200

    feat: add global styles for wrappers

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your course goal!' />
-        <Button title='Add Goal' />
-      </View>
-      <View>
-        <Text>List of goals</Text>
-      </View>
-    </View>
-  );
+    return (
+        <View style={appContainer}>
+            <View style={inputContainer}>
+                <TextInput style={textInput} placeholder='Your course goal!'/>
+                <Button title='Add Goal'/>
+            </View>
+            <View style={goalsContainer}>
+                <Text>List of goals</Text>
+            </View>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  appContainer: {
-    padding: 50
-  }
+const {
+    appContainer,
+    inputContainer,
+    textInput,
+    goalsContainer
+} = StyleSheet.create({
+    appContainer: {
+        flex: 1,
+        padding: 50,
+        paddingHorizontal: 16
+    },
+    inputContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc'
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#cccccc',
+        width: '70%',
+        marginRight: 8,
+        padding: 8
+    },
+    goalsContainer: {
+        flex: 5
+    }
 });
